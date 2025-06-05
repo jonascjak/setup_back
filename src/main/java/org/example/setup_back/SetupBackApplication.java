f4fbd07fc654c5978eb836a472f1e0869808f68f
jonascjak <jonascjak@gmail.com>
2025-06-06 00:29:38 +0200

    Initial commit

---
+++ src/main/java/org/example/setup_back/SetupBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.setup_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetupBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/setup_back/SetupBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.setup_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetupBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
